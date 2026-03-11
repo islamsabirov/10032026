a212102ec985788d5f09f17e72d86d4ce7fbb0dd
islamsabirov <sabirov0750@gmail.com>
2026-03-12 02:15:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -15,12 +15,15 @@ class Config:
     """Central configuration class"""
     
     # Telegram
-    BOT_TOKEN="BOT_TOKEN"
-    ADMIN_IDS="5907118746"
-    CHANNEL_ID="-1001898063437"
-    CHANNEL_LINK=https:"//t.me/kinolar040"
-    MONGODB_URI=mongodb:"//localhost:27017"
-    MONGB_NAME=movie_bot
+    BOT_TOKEN = "BOT_TOKEN"
+
+    ADMIN_IDS = [5907118746]
+
+    CHANNEL_ID = "-1001898063437"
+    CHANNEL_LINK = "https://t.me/kinolar040"
+
+    MONGODB_URI = "mongodb://localhost:27017"
+    MONGB_NAME = "movie_bot"
     # Redis
     REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))

daa94608146d9c30f7e829b3f8907c1d74309581
islamsabirov <sabirov0750@gmail.com>
2026-03-12 06:01:58 +0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,11 @@
-"""Utils paketi"""
+# utils/__init__.py
+# Bu fayl utils/ papkasini Python package'ga aylantiradi
 
-from .logger import Logger, logger
-from .helpers import (
-    # Kod funksiyalari
+from utils.logger import logger, Logger
+from utils.utils import (
     generate_code,
     validate_code,
     extract_code_from_text,
-    generate_short_code,
-    
-    # Formatlash
     format_number,
     format_file_size,
     format_time_ago,
@@ -16,43 +13,27 @@ from .helpers import (
     escape_markdown,
     escape_html,
     create_progress_bar,
-    
-    # Validatsiya
     is_valid_email,
     is_valid_url,
     is_valid_telegram_link,
     extract_username_from_link,
-    
-    # Xesh
     hash_string,
     generate_unique_id,
-    
-    # JSON
     safe_json_dumps,
     safe_json_loads,
-    
-    # Telegram UI
     build_menu,
     split_text,
     create_pagination_keyboard,
-    
-    # Dekoratorlar
     admin_only,
     owner_only,
     rate_limit,
-    
-    # Fayl
     ensure_dir,
     safe_filename,
-    
-    # Vaqt
     get_current_time,
     format_datetime,
     parse_date,
     get_start_of_day,
     get_end_of_day,
-    
-    # Xatolar
     BotError,
     UserNotFoundError,
     MovieNotFoundError,
@@ -63,66 +44,17 @@ from .helpers import (
 )
 
 __all__ = [
-    # Logger
-    'Logger',
-    'logger',
-    
-    # Kod funksiyalari
-    'generate_code',
-    'validate_code',
-    'extract_code_from_text',
-    'generate_short_code',
-    
-    # Formatlash
-    'format_number',
-    'format_file_size',
-    'format_time_ago',
-    'truncate_text',
-    'escape_markdown',
-    'escape_html',
-    'create_progress_bar',
-    
-    # Validatsiya
-    'is_valid_email',
-    'is_valid_url',
-    'is_valid_telegram_link',
-    'extract_username_from_link',
-    
-    # Xesh
-    'hash_string',
-    'generate_unique_id',
-    
-    # JSON
-    'safe_json_dumps',
-    'safe_json_loads',
-    
-    # Telegram UI
-    'build_menu',
-    'split_text',
-    'create_pagination_keyboard',
-    
-    # Dekoratorlar
-    'admin_only',
-    'owner_only',
-    'rate_limit',
-    
-    # Fayl
-    'ensure_dir',
-    'safe_filename',
-    
-    # Vaqt
-    'get_current_time',
-    'format_datetime',
-    'parse_date',
-    'get_start_of_day',
-    'get_end_of_day',
-    
-    # Xatolar
-    'BotError',
-    'UserNotFoundError',
-    'MovieNotFoundError',
-    'CodeNotFoundError',
-    'CodeAlreadyUsedError',
-    'SubscriptionRequiredError',
-    'handle_error',
+    "logger", "Logger",
+    "generate_code", "validate_code", "extract_code_from_text",
+    "format_number", "format_file_size", "format_time_ago",
+    "truncate_text", "escape_markdown", "escape_html", "create_progress_bar",
+    "is_valid_email", "is_valid_url", "is_valid_telegram_link", "extract_username_from_link",
+    "hash_string", "generate_unique_id",
+    "safe_json_dumps", "safe_json_loads",
+    "build_menu", "split_text", "create_pagination_keyboard",
+    "admin_only", "owner_only", "rate_limit",
+    "ensure_dir", "safe_filename",
+    "get_current_time", "format_datetime", "parse_date", "get_start_of_day", "get_end_of_day",
+    "BotError", "UserNotFoundError", "MovieNotFoundError",
+    "CodeNotFoundError", "CodeAlreadyUsedError", "SubscriptionRequiredError", "handle_error",
 ]
